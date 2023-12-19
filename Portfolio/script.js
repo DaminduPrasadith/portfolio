e3931fc1ebe7bcc91ef1f65ff432e663e93eba7a
Damindu Prasadith <139045998+DaminduPrasadith@users.noreply.github.com>
2023-12-19 17:36:44 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,16 @@
+
+
+
+function slidbar(){
+    const anothervavBar = document.querySelector('#anothervavBar');
+    anothervavBar.style.display = 'grid';
+}
+function slidbarhidden(){
+    const anothervavBar = document.querySelector('#anothervavBar');
+    anothervavBar.style.display = 'none';
+}
+
+function hlink(){
+    const anothervavBar = document.querySelector('#anothervavBar');
+    anothervavBar.style.display = 'none';
+}
